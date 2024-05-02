84a2f4dff9281bd077b7b676d0e5fd8e2de58ac9
SallyPJ <polonio.sally@gmail.com>
2024-05-02 18:33:37 +0200

    - Ajout d'un script Category fonctionnel

--- scrapers/category.py
+++ scrapers/category.py
@@ -1,10 +1,9 @@
-import main
-from main import get_book_info, write_to_csv
+from book_info import get_book_info, write_to_csv
 import requests
 from bs4 import BeautifulSoup
-#import urllib3
 import urllib.parse
 import csv
+
 
 
 cat_url = "https://books.toscrape.com/catalogue/category/books/fiction_10/index.html"
@@ -14,38 +13,50 @@ response = requests.get(cat_url)
 soup = BeautifulSoup(response.text, "html.parser")
 books_link_list =[]
 all_data = []
-
+url_list = []
 #Récupère les urls des livres sur une page
-def retrieve_book_urls(cat_url):
-    product_articles = soup.find_all("article", class_="product_pod")
-    for article in product_articles:
-        h3_tag = article.find("h3")
-        link = h3_tag.find("a").get("href")
-        parts = link.split('/')
-        book_partial_url = parts[-2] + '/' + parts[-1]
-        print(book_partial_url)
-        book_full_url = urllib.parse.urljoin(book_base_url, book_partial_url)
-        books_link_list.append(book_full_url)
+
+
+#Récupère la liste des urls des livres de chaque page d'une catégorie
+def retrieve_book_urls(url_list):
+    for url in url_list:
+        response2 = requests.get(url)
+        soup2 = BeautifulSoup(response2.text, "html.parser")
+        product_articles = soup2.find_all("article", class_="product_pod")
+        for article in product_articles:
+            h3_tag = article.find("h3")
+            link = h3_tag.find("a").get("href")
+            parts = link.split('/')
+            book_partial_url = parts[-2] + '/' + parts[-1]
+            print(book_partial_url)
+            book_full_url = urllib.parse.urljoin(book_base_url, book_partial_url)
+            books_link_list.append(book_full_url)
     print(books_link_list)
     return books_link_list
-
 #Récupère l'url d'une page Next (en cours)
-def search_for_next():
+
+def search_for_next(url_cat):
+
+    url_list.append(cat_url)
     next_button = soup.find("li", class_="next")
-    print(next_button)
-    if next_button:
+    while next_button:
         next_partial_url = next_button.find("a").get("href")
-        next_full_url = urllib.parse.urljoin(next_base_url,next_partial_url)
+        next_full_url = urllib.parse.urljoin(next_base_url, next_partial_url)
+        url_list.append(next_full_url)
         print(next_partial_url)
         print(next_full_url)
-
-    else :
-        print("Balise li non trouvée")
+        response1 = requests.get(next_full_url)
+        soup1 = BeautifulSoup(response1.text, "html.parser")
+        next_button = soup1.find("li", class_="next")
+    if not next_button:
+        print("Il n'y a plus de bouton 'next'.")
+    print(url_list)
+    return url_list
 
 #Récupère les urls des livres sur une page
 def scrap_cat(books_link_list):
     for url in books_link_list:
-        data = main.get_book_info(url)
+        data = get_book_info(url)
         all_data.append(data)
     write_to_csv1(all_data, "../cat.csv")
 
@@ -62,6 +73,7 @@ def write_to_csv1(data_list, filename):
 
 
 
-retrieve_book_urls(cat_url)
-search_for_next()
+
+search_for_next(cat_url)
+retrieve_book_urls(url_list)
 scrap_cat(books_link_list)
